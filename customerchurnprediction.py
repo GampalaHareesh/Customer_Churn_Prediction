5fad8c01ecbb82de99acc42ef662f37a30cb204e
GampalaHareesh <145988269+GampalaHareesh@users.noreply.github.com>
2023-09-27 19:31:31 +0530

    Add files via upload

---
+++ customerchurnprediction.py
@@ -0,0 +1,47 @@
+# Import necessary libraries
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+
+# Load your customer churn dataset (replace 'dataset.csv' with your dataset file)
+data = pd.read_csv('dataset.csv')
+
+# Explore the dataset (optional)
+# print(data.head())
+
+# Define feature columns and target variable
+X = data.drop('Churn', axis=1)  # Features
+y = data['Churn']  # Target variable
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Standardize the features (optional but recommended)
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+# Create a logistic regression model
+model = LogisticRegression(random_state=42)
+
+# Fit the model on the training data
+model.fit(X_train, y_train)
+
+# Make predictions on the test data
+y_pred = model.predict(X_test)
+
+# Evaluate the model
+accuracy = accuracy_score(y_test, y_pred)
+conf_matrix = confusion_matrix(y_test, y_pred)
+classification_rep = classification_report(y_test, y_pred)
+
+print(f'Accuracy: {accuracy:.2f}')
+print('Confusion Matrix:')
+print(conf_matrix)
+print('Classification Report:')
+print(classification_rep)
+
+# You can now use this model to predict customer churn for new data
